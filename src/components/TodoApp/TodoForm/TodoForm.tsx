b105a6478a06ffafd102c8f9712a64fea24a9165
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-03-16 17:15:14 +0200

    added form validation

--- src/components/TodoApp/TodoForm/TodoForm.tsx
+++ src/components/TodoApp/TodoForm/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import './TodoForm.scss';
 
@@ -8,23 +8,31 @@ interface TodoFormProps {
 
 const TodoForm: React.FC<TodoFormProps> = ({ createNewItem }) => {
     const [userInput, setUserInput] = useState<string>('');
+    const form = useRef<HTMLFormElement | null>(null);
+    const minCharactersNumber = 4,
+          maxCharacterNumber = 100;
 
     const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        if (userInput) {
+        if (userInput.trim().length >= minCharactersNumber && userInput.length <= maxCharacterNumber) {
             createNewItem(userInput);
             setUserInput('');
+        } else {
+            form.current?.classList.add('invalid');
         }
     };
 
     const inputHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        form.current?.classList.remove('invalid');
         setUserInput(e.currentTarget.value);
     };
 
     return (
-        <form onSubmit={submitHandler} className='todo-form' action="#">
+        <form ref={form} onSubmit={submitHandler} className='todo-form' action="#">
             <input placeholder='Create a new todo...' onInput={inputHandler} value={userInput} type="text"/>
+            <span
+                className='todo-form__invalid-label'>Tasks must contain at least {minCharactersNumber} and not more {maxCharacterNumber} characters</span>
         </form>
     );
 
