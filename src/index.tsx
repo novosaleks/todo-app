a45322d7ef5f7dfbd0f18751a8885099c4ead4cf
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-03-16 13:38:41 +0200

    Added favicon

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import favicon from './assets/images/favicon-32x32.png';
+import Favicon from 'react-favicon';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Favicon url={favicon}/>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
